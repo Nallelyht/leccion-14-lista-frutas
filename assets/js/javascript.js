093fe7e8df7e932bf842be930b233d946e74971a
Nallely House Thomas <Nallely House Thomas>
2017-03-20 20:22:49 -0600

    corrigiendo faltas de ortografia e identacion

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,15 +1,12 @@
 
-function agregarFruta()
-{
+function agregarFruta(){
     var nuevaFruta=document.getElementById("nuevaFruta").value;
     if(nuevaFruta.length>0)
     {
-
-          var fruta=document.createElement('li');
-            fruta.id=nuevaFruta;
-            fruta.innerHTML=nuevaFruta;
-            document.getElementById("listaOrdenada").appendChild(fruta);
-
+      var fruta=document.createElement('li');
+      fruta.id=nuevaFruta;
+      fruta.innerHTML=nuevaFruta;
+      document.getElementById("listaOrdenada").appendChild(fruta);
     }
     return false;
 }
